2ae8b94a3e912c6326d2080751b83723b401a72b
Renan Souza <renantrsouza@gmail.com>
2022-03-28 11:30:13 -0400

    Teste para saber se a funçao e chamada

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, fireEvent} from '@testing-library/react'
 import Conta from './Conta'
+
 describe('Componente de conta', () => {
     it('Exibir o saldo da conta como valor monetário', () => {
         render(<Conta saldo={1000} />)
 
         const saldo = screen.getByTestId('saldo-conta')
         expect(saldo.textContent).toBe('R$ 1000')
+    })
+    it('Chama a função de realizar transação, quando o botão é clicado', () => {
+        const funcaoRealizarTransacao = jest.fn() //identifica para o jest que é uma função
+
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao} />)
+
+        fireEvent.click(screen.getByText('Realizar operação')) //Click do botao
+
+        expect(funcaoRealizarTransacao).toHaveBeenCalled() //checa se a função foi chamada
     })
 })
